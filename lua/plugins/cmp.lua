5f653d67996f11cc4e20a5c75f64cb021c0d061b
Hiram Red <1165107440@qq.com>
2026-04-29 23:24:49 +0800

    feat: update <tab> action in cmp popup window

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -30,15 +30,15 @@ return {
           ["<C-Space>"] = cmp.mapping.complete(),
           -- ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
+          ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+          --   if cmp.visible() then
+          --     cmp.mapping.confirm({ select = true })
+          --   elseif luasnip.expand_or_jumpable() then
+          --     luasnip.expand_or_jump()
+          --   else
+          --     fallback()
+          --   end
+          -- end, { "i", "s" }),
           ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()

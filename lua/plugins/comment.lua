5d4cb8ec9b50404ca8c6c2da17489fe91faa0186
Hiram Red <1165107440@qq.com>
2026-04-30 00:04:05 +0800

    feat: new vue comment support

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -14,29 +14,8 @@ return {
       "JoosepAlviste/nvim-ts-context-commentstring",
     },
     config = function()
-      local function get_vue_commentstring()
-        local line = vim.api.nvim_get_current_line()
-        local cursor = vim.api.nvim_win_get_cursor(0)
-        local lnum = cursor[1] - 1
-        local col = cursor[2]
-
-        -- Get all lines up to cursor
-        local lines = vim.api.nvim_buf_get_lines(0, 0, lnum, false)
-        local content = table.concat(lines, "\n")
-
-        -- Check if in <script> tag
-        if content:match("<script") and not content:match("</script>") then
-          return "// %s"
-        end
-
-        -- Check if in <style> tag
-        if content:match("<style") and not content:match("</style>") then
-          return "/* %s */"
-        end
-
-        -- Default to HTML comment for <template>
-        return "<!-- %s -->"
-      end
+      -- need tree-sitter-vue support
+      -- npm install -g tree-sitter-vue tree-sitter
       require('Comment').setup({
 
         padding = true,
@@ -53,13 +32,7 @@ return {
           basic = true,
           extra = false,
         },
-        ---Function to call before (un)comment
-        pre_hook = function(ctx)
-          local ft = vim.bo.filetype
-          if ft == "vue" then
-            return get_vue_commentstring()
-          end
-        end,
+        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
     })
     end,
   },

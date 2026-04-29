d005a7dc39b37d233fb5f48c6bdc80d2174c0a72
Hiram Red <1165107440@qq.com>
2026-04-29 20:15:56 +0800

    feat: add vue support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,27 +11,56 @@ return {
     config = function()
       require("mason").setup()
       require("mason-lspconfig").setup({
-        ensure_installed = { "ts_ls" },
+        ensure_installed = { "vtsls", "vue_ls" },
       })
 
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      -- TypeScript/JavaScript (ts_ls is the new name for tsserver)
-      vim.lsp.config("ts_ls", {
+      -- Vue (vue_ls)
+      vim.lsp.config("vue_ls", {
         capabilities = capabilities,
       })
-      vim.lsp.enable("ts_ls")
+
+      -- 配置vtsls以支持Vue
+      local vue_language_server_path = vim.fn.stdpath('data') .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+      local vue_plugin = {
+        name = '@vue/typescript-plugin',
+        location = vue_language_server_path,
+        languages = { 'vue' },
+        configNamespace = 'typescript',
+      }
+
+      -- TypeScript/JavaScript (ts_ls is the new name for tsserver)
+      vim.lsp.config("vtsls", {
+        capabilities = capabilities,
+        filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+        settings = {
+          vue = {
+            target = 2,
+          },
+          vtsls = {
+            tsserver = {
+              globalPlugins = {
+                vue_plugin,
+              },
+            },
+          },
+        },
+      })
+
+      -- Enable LSPs
+      vim.lsp.enable("vtsls", "vue_ls")
 
       -- LSP key mappings
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
-      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+      vim.keymap.set("n", "<leader>u", vim.lsp.buf.declaration, { desc = "Go to declaration" })
       vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go to references" })
-      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
+      vim.keymap.set("n", "<leader>i", vim.lsp.buf.implementation, { desc = "Go to implementation" })
       vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation" })
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
       vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
-      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
-      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+      vim.keymap.set("n", "[g", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
+      vim.keymap.set("n", "]g", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
     end,
   },
 

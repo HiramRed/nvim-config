a5875dfe48a69c3360f7dfcd8008223a2f493be5
Hiram Red <1165107440@qq.com>
2026-04-30 01:07:01 +0800

    feat: update nvim-tree setting

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -9,8 +9,8 @@ local function my_on_attach(bufnr)
     api.map.on_attach.default(bufnr)
 
     -- custom mappings
-    vim.keymap.set("n", "<C-t>", api.tree.change_root_to_parent,        opts("Up"))
-    vim.keymap.set('n', '<leader>ss', ':NvimTreeFindFile<CR>', { noremap = true, silent = true })
+    -- vim.keymap.set("n", "P", api.fs.paste(), opts("Paste"))
+    -- vim.keymap.set("n", "p", api.node.navigate.parent(), opts("Parent"))
     -- vim.keymap.set("n", "?",     api.tree.toggle_help,                  opts("Help"))
 end
 
@@ -26,6 +26,7 @@ return {
         -- NvimTree keymap
         require('nvim-tree').setup(opts)
         vim.keymap.set('n', '<leader>e', ':NvimTreeOpen<cr>', { silent = true })
+        vim.keymap.set('n', '<leader>ss', ':NvimTreeFindFile<CR>', { noremap = true, silent = true })
       end,
       opts = {
           auto_reload_on_write = true,
@@ -49,49 +50,6 @@ return {
             number = false,
             relativenumber = false,
             signcolumn = "yes",
-            -- mappings = {
-            --   custom_only = false,
-            --   list = {
-            --     -- user mappings go here
-            --     { key = {"<CR>", "o", "<2-LeftMouse>"}, action = "edit" },
-            --     { key = "<C-e>",                        action = "edit_in_place" },
-            --     { key = {"O"},                          action = "edit_no_picker" },
-            --     { key = {"<2-RightMouse>", "<C-]>"},    action = "cd" },
-            --     { key = "<C-v>",                        action = "vsplit" },
-            --     { key = "<C-x>",                        action = "split" },
-            --     { key = "<C-t>",                        action = "tabnew" }, { key = "<",                            action = "prev_sibling" },
-            --     { key = ">",                            action = "next_sibling" },
-            --     { key = "p",                            action = "parent_node" },
-            --     { key = "<BS>",                         action = "close_node" },
-            --     { key = "<Tab>",                        action = "preview" },
-            --     { key = "K",                            action = "first_sibling" },
-            --     { key = "J",                            action = "last_sibling" },
-            --     { key = "I",                            action = "toggle_git_ignored" },
-            --     { key = "H",                            action = "toggle_dotfiles" },
-            --     { key = "R",                            action = "refresh" },
-            --     { key = "a",                            action = "create" },
-            --     { key = "d",                            action = "remove" },
-            --     { key = "D",                            action = "trash" },
-            --     { key = "r",                            action = "rename" },
-            --     { key = "<C-r>",                        action = "full_rename" },
-            --     { key = "x",                            action = "cut" },
-            --     { key = "c",                            action = "copy" },
-            --     { key = "P",                            action = "paste" },
-            --     { key = "y",                            action = "copy_name" },
-            --     { key = "Y",                            action = "copy_path" },
-            --     { key = "gy",                           action = "copy_absolute_path" },
-            --     { key = "[c",                           action = "prev_git_item" },
-            --     { key = "]c",                           action = "next_git_item" },
-            --     { key = "-",                            action = "dir_up" },
-            --     { key = "s",                            action = "system_open" },
-            --     { key = "q",                            action = "close" },
-            --     { key = "g?",                           action = "toggle_help" },
-            --     { key = "W",                            action = "collapse_all" },
-            --     { key = "S",                            action = "search_node" },
-            --     { key = "<C-k>",                        action = "toggle_file_info" },
-            --     { key = ".",                            action = "run_file_command" }
-            --   },
-            -- },
           },
           update_focused_file = {
             enable = false,
@@ -134,7 +92,7 @@ return {
               quit_on_open = false,
               resize_window = false,
               window_picker = {
-                enable = true,
+                enable = false,
                 chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
                 exclude = {
                   filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },

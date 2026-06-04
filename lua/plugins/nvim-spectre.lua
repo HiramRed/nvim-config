44a560822f837a5d26b3d387ed871bd1e617cb20
Hiram Red <1165107440@qq.com>
2026-06-05 00:34:46 +0800

    更新keymap

--- lua/plugins/nvim-spectre.lua
+++ lua/plugins/nvim-spectre.lua
@@ -2,6 +2,9 @@ return {
   -- 加上 spectre 专门处理 rg 搜索替换
   {
     "nvim-pack/nvim-spectre",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
     keys = {
       { "<leader>S", '<cmd>lua require("spectre").toggle()<cr>', desc = "全局搜索替换" },
     },

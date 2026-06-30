9375e412c42f2757fb5b87df09f7a1ba1ab8c01f
Hiram Red <1165107440@qq.com>
2026-06-30 12:32:30 +0800

    feat: 添加text_indent_obj插件，支持vii快速选择同缩进内容

--- lua/plugins/nvim-treesitter-textobjects.lua
+++ lua/plugins/nvim-treesitter-textobjects.lua
@@ -6,12 +6,6 @@ return {
       -- Disable entire built-in ftplugin mappings to avoid conflicts.
       -- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
       vim.g.no_plugin_maps = true
-
-      -- Or, disable per filetype (add as you like)
-      -- vim.g.no_python_maps = true
-      -- vim.g.no_ruby_maps = true
-      -- vim.g.no_rust_maps = true
-      -- vim.g.no_go_maps = true
     end,
     config = function()
       -- configuration
@@ -20,35 +14,16 @@ return {
           set_jumps = true,
         },
         select = {
-          -- Automatically jump forward to textobj, similar to targets.vim
           lookahead = true,
-          -- You can choose the select mode (default is charwise 'v')
-          --
-          -- Can also be a function which gets passed a table with the keys
-          -- * query_string: eg '@function.inner'
-          -- * method: eg 'v' or 'o'
-          -- and should return the mode ('v', 'V', or '<c-v>') or a table
-          -- mapping query_strings to modes.
           selection_modes = {
-            ['@parameter.outer'] = 'v', -- charwise
-            ['@function.outer'] = 'V', -- linewise
-            -- ['@class.outer'] = '<c-v>', -- blockwise
+            ['@parameter.outer'] = 'v',
+            ['@function.outer'] = 'V',
           },
-          -- If you set this to `true` (default is `false`) then any textobject is
-          -- extended to include preceding or succeeding whitespace. Succeeding
-          -- whitespace has priority in order to act similarly to eg the built-in
-          -- `ap`.
-          --
-          -- Can also be a function which gets passed a table with the keys
-          -- * query_string: eg '@function.inner'
-          -- * selection_mode: eg 'v'
-          -- and should return true of false
           include_surrounding_whitespace = false,
         },
       }
 
       -- keymaps
-      -- You can use the capture groups defined in `textobjects.scm`
       vim.keymap.set("n", "]a", function()
         require("nvim-treesitter-textobjects.swap").swap_next "@parameter.inner"
       end)
@@ -67,7 +42,6 @@ return {
       vim.keymap.set({ "x", "o" }, "ic", function()
         require "nvim-treesitter-textobjects.select".select_textobject("@class.inner", "textobjects")
       end)
-      -- You can also use captures from other query groups like `locals.scm`
       vim.keymap.set({ "x", "o" }, "as", function()
         require "nvim-treesitter-textobjects.select".select_textobject("@local.scope", "locals")
       end)
@@ -95,10 +69,10 @@ return {
       vim.keymap.set({ "n", "x", "o" }, "[[", function()
         require("nvim-treesitter-textobjects.move").goto_previous_start("@class.outer", "textobjects")
       end)
-      -- You can also use captures from other query groups like `locals.scm`
-      vim.keymap.set({ "x", "o" }, "as", function()
-        require "nvim-treesitter-textobjects.select".select_textobject("@local.scope", "locals")
-      end)
     end,
-  }
+  },
+  {
+    "michaeljsmith/vim-indent-object",
+    lazy = false,
+  },
 }
